8302bd4d19ddf9974709eeaf3188de0b31b12f08
Yunze Xu <xyzinfernity@163.com>
2022-10-14 19:30:58 +0800

    Add a script to set up all things

--- README.md
+++ README.md
@@ -2,13 +2,35 @@
 
 The development environment based on Vim editor.
 
-## Build the image
+## Install on Ubuntu 20.04
+
+You only need to run `./install.sh`.
+
+Since GitHub and apt source are blocked in some particular areas, if you have a proxy, you can configure the environment variable `PROXY` before running `install.sh`.
+
+For example, my Ubuntu runs in [WSL2](https://learn.microsoft.com/en-us/windows/wsl/install) and there is a proxy that listens on 10808 port. Here are how I configured the `PROXY` variable:
+
+```bash
+# Inside WSL2, run `ipconfig` on Windows to see the actual IP in your env
+export PROXY=socks5://172.30.80.1:10808
+```
+
+```bash
+# Inside a ubuntu:20.04 docker container
+export PROXY=socks5://host.docker.internal:10808
+```
+
+## Docker image set up
+
+You can also set up the development environment in a Docker image. Here are the instructions to build a Ubuntu-based image with Vim and the plugins installed.  
+
+### Build the image
 
 ```bash
 docker build -t ubuntu-18.04-vim-dev .
 ```
 
-## Example
+### Example
 
 After building the image successfully, you can edit [the C++ example file](./example.cc) when starting the container.
 
---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -ex
+cd ~
+
+if [[ $USER != "" && $USER != "root" ]]; then
+    SUDO=sudo
+fi
+
+if [[ $PROXY ]]; then
+    export https_proxy=$PROXY
+fi
+
+$SUDO apt -y update
+$SUDO apt install -y curl git gcc g++ cmake clang-format ccls nodejs universal-ctags
+
+if [[ $PROXY ]]; then
+    git config --global http.https://github.com.proxy $PROXY
+fi
+
+# ccls required Node.js 14.14.0 or higher
+curl -O -L https://install-node.vercel.app/lts
+bash lts -y
+rm -f lts
+
+$SUDO apt install -y libncurses-dev python3-dev
+git clone https://github.com/vim/vim.git -b v9.0.0713
+pushd vim
+    LDFLAGS=-rdynamic ./configure --enable-python3interp --with-python3-command=python3
+    make -j4
+    $SUDO make install
+popd
+
+git clone https://github.com/BewareMyPower/vim-dev-env.git
+pushd vim-dev-env
+    cp .vimrc ~/.vimrc
+    
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
+    mkdir -p ~/.vim/plugged
+    pushd ~/.vim/plugged
+        git clone https://github.com/skywind3000/asyncrun.vim.git
+        git clone https://github.com/neoclide/coc.nvim.git -b release
+        git clone https://github.com/Yggdroot/LeaderF.git
+        git clone https://github.com/preservim/nerdtree.git
+        git clone https://github.com/mhinz/vim-signify.git
+        git clone https://github.com/vim-scripts/a.vim.git
+        git clone https://github.com/vim-airline/vim-airline.git
+        git clone https://github.com/vim-airline/vim-airline-themes.git
+    popd
+
+    cp -f .vimrc ~/.vimrc
+    cp -f coc-settings.json ~/.vim
+popd
